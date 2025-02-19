69fd4466f317ef7ebfff1b19936ca83535eb2bd7
rober <rober@ROBERT-PC>
2025-02-19 17:57:37 +0100

    Add project files.

---
+++ FileExplorerTest/Models/FileItem.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+public class FileItem : INotifyPropertyChanged
+{
+    private bool _isSelected;
+
+    public bool IsSelected
+    {
+        get => _isSelected;
+        set
+        {
+            if (_isSelected != value)
+            {
+                _isSelected = value;
+                OnPropertyChanged(nameof(IsSelected));
+            }
+        }
+    }
+
+    public string Path { get; set; }
+
+    public string FileName => System.IO.Path.GetFileName(Path);
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ FileExplorerTest/ViewModels/FilesListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FileExplorerTest.ViewModels
+{
+    public class FilesListViewModel
+    {
+        public ObservableCollection<FileItem> Files { get; set; }
+
+        public FilesListViewModel()
+        {
+            Files = new ObservableCollection<FileItem>
+        {
+            new FileItem { Path = @"D:\Folder\plik1.png" },
+            new FileItem { Path = @"D:\Folder\plik2.png" },
+            new FileItem { Path = @"D:\Folder\plik3.png" }
+        };
+        }
+    }
+}
